37621842f4ea15069817fd4ae847197e8fdb2335
Edu Andrade <eduardo.a@edu.pucrs.br>
2022-09-29 08:09:17 -0300

    Added letter coincidence index and copied first solution

--- Program.cs
+++ Program.cs
@@ -4,6 +4,22 @@ namespace T1_Criptoanalise;
 
 public class Program
 {
+    public static string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    public static double[] EnFreq =
+    {
+        0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228, 0.02015, 0.06094, 0.06966, 0.00153, 0.00772, 0.04025,
+        0.02406, 0.06749, 0.07507, 0.01929, 0.00095, 0.05987, 0.06327, 0.09056, 0.02758, 0.00978, 0.0236, 0.0015,
+        0.01974, 0.00074
+    };
+
+    public static double[] PtFreq =
+    {
+        0.14634, 0.01043, 0.03882, 0.04992, 0.1257, 0.01023, 0.01303, 0.00781, 0.06186, 0.00397, 0.00015, 0.02779,
+        0.04738, 0.04446, 0.09735, 0.02523, 0.01204, 0.0653, 0.06805, 0.04336, 0.03639, 0.01575, 0.00037, 0.00253,
+        0.00006, 0.0047
+    };
+
     public static void Main(string[] args)
     {
         Console.WriteLine(CalculateDistances("Dados/Textos cifrados/cipher1.txt"));
@@ -11,21 +27,20 @@ public class Program
 
     public static double CalculateDistances(string fileName)
     {
-        var textCombinations = new ConcurrentDictionary<string, int>();
+        var letterFrequencies = new ConcurrentDictionary<char, int>();
         string text = File.ReadAllText(fileName);
 
-        Parallel.For(0, text.Length - 3, i => { textCombinations.AddOrUpdate(text[i..(i + 3)], 1, (_, v) => v + 1); });
+        Parallel.For(0, text.Length, i => { letterFrequencies.AddOrUpdate(text[i], 1, (_, v) => v + 1); });
 
         double sum = 0;
-        foreach (int frequency in textCombinations.Values)
+        foreach (int frequency in letterFrequencies.Values)
         {
-            sum += frequency * (frequency - 1);
+            sum += frequency * (frequency - 1) / ((double)text.Length * (text.Length - 1));
         }
 
-        double coincidenceIndex = sum / ((long)text.Length * (text.Length - 1));
+        //double coincidenceIndex = sum / ((long)text.Length * (text.Length - 1));
 
-        
-        
+
         return 0;
     }
 
